98d69307a02832ad6d296ac5bddee43069d84fe7
tadeo <tadeoacosta14@gmail.com>
2024-06-10 22:03:18 -0300

    estamos creando componentes y utilizando react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   const [count, setCount] = useState(0)
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Nav } from './Nav.jsx'
 
+import { Navbar } from '../src/componentes/Navbar.jsx'
+import { Registro } from './componentes/Registro.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav />
+    <Registro/>
   </React.StrictMode>,
 )
